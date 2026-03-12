7fac2d13fdb7d53839482169e2f10d5edde7187d
IndraHfid <indrahafidsaputra@gmail.com>
2026-03-12 14:43:33 +0700

    buat main class KtpCrudApplication dengan SpringBootApplication

---
+++ src/test/java/com/example/demo/KtpCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
